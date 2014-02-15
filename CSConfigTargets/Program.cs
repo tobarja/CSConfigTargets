e82d65b168a1ef7a90e5dca638ef504a1135dc9d
Andrew Thompson <andrew@tobarja.com>
2014-02-15 15:24:54 -0500

    Dev, Test, Prod Build Configs

--- CSConfigTargets/Program.cs
+++ CSConfigTargets/Program.cs
@@ -10,6 +10,16 @@ namespace CSConfigTargets
     {
         static void Main(string[] args)
         {
+            string Config;
+#if (PROD)
+            Config = "Production";
+#elif (TEST)
+            Config = "Testing";
+#else
+            Config = "Development";
+#endif
+            Console.WriteLine(Config);
+            Console.ReadKey();
         }
     }
 }
